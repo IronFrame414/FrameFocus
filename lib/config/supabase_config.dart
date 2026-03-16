58f83c459a351fa5839679587cb33407c73533de
IronFrame414 <josh@worthprop.com>
2026-03-16 02:02:38 -0400

    Update Supabase anonKey with new value

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -2,7 +2,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
   static const String url = 'https://dbmjqetqtgbfaljlhbzj.supabase.co';
-  static const String anonKey = 'YOUR-ANON-PUBLIC-KEY';
+  static const String anonKey = 'sb_publishable_Do2kWRRwvqGfmT8QBXu7bw_UR9OutiG';
 
   static SupabaseClient get client => Supabase.instance.client;
 
